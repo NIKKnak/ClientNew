db4f8968694827b5ee09c6df4282747cc2b4ca03
Николай Ветошкин <nik88350759@yandex.ru>
2023-12-17 19:44:39 +0300

    first commit1

---
+++ Desktop/C# start/Client/Message.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MyClient
+{
+    public class Message
+    {
+
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; }
+        public string NiknameFrom { get; set; }
+        public string NiknameTo { get; set; }
+
+        public string SerializeMessageToJson() => JsonSerializer.Serialize(this);
+
+        public static Message? DesirializeFromJson(string message) => JsonSerializer.Deserialize<Message>(message);
+
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"{this.DateTime} получено сообщение {this.Text} от {this.NiknameFrom}";
+        }
+
+    }
+}
